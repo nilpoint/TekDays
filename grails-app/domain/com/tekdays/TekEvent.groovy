1b4329972679cdc6e50580c297a42b60d9e1ccd3
john <donghai.li@alstru.com>
2014-06-16 14:24:07 +0800

    Add domain class TekEvent.

---
+++ grails-app/domain/com/tekdays/TekEvent.groovy
@@ -0,0 +1,26 @@
+package com.tekdays
+
+class TekEvent {
+
+  String city
+  String name
+  String organizer
+  String venue
+  Date startDate
+  Date endDate
+  String description
+
+    static constraints = {
+      name()
+      city()
+      description maxSize: 5000
+      organizer()
+      venue()
+      startDate()
+      endDate()
+    }
+
+  String toString() {
+    "$name, $city"
+  }
+}
